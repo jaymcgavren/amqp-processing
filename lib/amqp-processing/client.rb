8a0e4fbedb411f7e24c52fbee94aa9799b914ad3
Jay McGavren <jay@mcgavren.com>
2010-12-10 22:33:36 -0700

    Client#app= now undefs insecure methods on app.

--- lib/amqp-processing/client.rb
+++ lib/amqp-processing/client.rb
@@ -12,7 +12,7 @@ class Client
   attr_reader :app
   def app=(value)
     @app = value
-    #undef_insecure_methods(@app)
+    undef_insecure_methods(@app)
   end
   
   #The server connection Thread, set by the connect method.
@@ -52,6 +52,42 @@ class Client
     @call_fanout.publish(Marshal.dump([@client_id, name, arguments]))
     return_value
   end
+  
+  private
+  
+    def undef_insecure_methods(object)
+      class << object
+        %w{
+          clone
+          define_singleton_method
+          display
+          dup
+          extend
+          freeze
+          instance_eval
+          instance_exec
+          instance_variable_defined?
+          instance_variable_get
+          instance_variable_set
+          instance_variables
+          public_send
+          remove_instance_variable
+          save
+          send
+          tap
+          library_loaded?
+          load_java_library
+          load_libraries
+          load_library
+          load_ruby_library
+        }.each do |method|
+          if method_defined?(method.to_sym)
+            undef_method(method.to_sym)
+          end
+        end
+      end
+    end
+  
   
   
 end
